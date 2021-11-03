b5641c89cef79f120d554a357099103a09cddb0e
Jan <jan.steimann@hhu.de>
2021-11-03 09:08:51 +0100

    Get airport tags only if lookingForAirports is true

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,7 +1,11 @@
 console.log("Content Script for Your Flight Injected");
 
-document.addEventListener("click", (event) => {
-    let stopText = event.target.innerText;
-    let airportTag = stopText.split("[")[1].split("]")[0];
-    console.log(airportTag);
-});
+let lookingForAirports = false;
+
+if (lookingForAirports) {
+    document.addEventListener("click", (event) => {
+        let stopText = event.target.innerText;
+        let airportTag = stopText.split("[")[1].split("]")[0];
+        console.log(airportTag);
+    });
+}
