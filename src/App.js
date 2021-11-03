725d1bb13055cf3b301281372b47220d4139a37d
Jan <jan.steimann@hhu.de>
2021-11-03 08:55:39 +0100

    Render default list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 /*global chrome*/
-import logo from './logo.svg';
 import './App.css';
+import AirportList from "./Components/AirportList";
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Woah it's a React Chrome extension...</p>
-        <a
-          className='App-link'
-          href='https://developer.chrome.com/docs/extensions/mv3/getstarted/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          View Extension Dev Docs
-        </a>
-      </header>
-    </div>
+      <div>
+        <AirportList />
+      </div>
   );
 }
 
