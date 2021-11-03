dd39637b1dac6bd4d636a8194f5a81cd2f14f5d3
Jan <jan.steimann@hhu.de>
2021-11-03 08:45:19 +0100

    Add event listener to get airport tags by click

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,1 +1,7 @@
 console.log("Content Script for Your Flight Injected");
+
+document.addEventListener("click", (event) => {
+    let stopText = event.target.innerText;
+    let airportTag = stopText.split("[")[1].split("]")[0];
+    console.log(airportTag);
+});
