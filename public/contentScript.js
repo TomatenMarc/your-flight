0ae4595e954216fb8580ece0db9fc8870baa55f0
Jan <jan.steimann@hhu.de>
2021-11-05 14:28:15 +0100

    Read airport tags on change

--- public/contentScript.js
+++ public/contentScript.js
@@ -2,20 +2,15 @@
 
 console.log("Content Script for Your Flight Injected");
 
-function sendAirportTags(airportTag){
+function sendAirportTags(airportTag) {
     chrome.runtime.sendMessage({message: "airportTags", airportTag: airportTag});
 }
 
-chrome.runtime.onMessage.addListener(
-    function (request) {
-        if (request.message === 'get_airports') {
-            console.log("get_airports");
-            document.addEventListener("click", (event) => {
-                let stopText = event.target.innerText;
-                let airportTag = stopText.split("[")[1].split("]")[0];
-                console.log(airportTag);
-                sendAirportTags(airportTag);
-            });
-        }
-    }
-);
+
+function getAirportTags (event) {
+    let element = document.getElementsByClassName("jb-js-checked")[0].parentNode.getElementsByClassName("jb-searchresult-connection-detail")[0].innerText
+    console.log(element);
+}
+
+window.onchange = getAirportTags();
+
